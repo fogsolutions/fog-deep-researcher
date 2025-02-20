0cc82557416730445cedff971573156899468f79
Kevin Booth <kevin@fogsolutions.com>
2025-02-20 18:01:51 -0500

    Updating deployment

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,8 @@
 
 RESROURCE_GROUP=${RESOURCE_GROUP:="rg-fog-demo-dev"}
 
-IMAGE_NAME="crz3zicizv4obfi.azurecr.io/gpt-researcher:latest"
+APP_NAME="gpt-researcher"
+IMAGE_NAME="crz3zicizv4obfi.azurecr.io/${APP_NAME}:latest"
 USER_ASSIGNED_IDENTITY_ID="/subscriptions/090002d9-f46e-4923-9960-156e86ba8095/resourcegroups/rg-fog-demo-dev/providers/Microsoft.ManagedIdentity/userAssignedIdentities/id-apps-z3zicizv4obfi"
 USER_ASSIGNED_IDENTITY_NAME="id-apps-z3zicizv4obfi"
 CONTAINER_APPS_ENV="cae-z3zicizv4obfi-01"
@@ -15,7 +16,7 @@ TARGET_PORT=8000
 # docker push $IMAGE_NAME
 # ./build.sh
 
-az containerapp up --name gpt-researcher \
+az containerapp up --name ${APP_NAME} \
     --resource-group $RESOURCE_GROUP \
     --environment "${CONTAINER_APPS_ENV}" \
     --env-vars $(awk -F= '{printf "%s=%s ", $1, $2}' ./.env) \
